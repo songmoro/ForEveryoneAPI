e1e8d1e1e956c04d89dc5adfc9fcfee66511f785
songmoro <wlqgkr@naver.com>
2023-11-06 22:30:31 +0900

    Initial Commit

---
+++ ForEveryoneAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForEveryoneAPI
+//
+//  Created by 송재훈 on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForEveryoneAPI/ForEveryoneAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEveryoneAPIApp.swift
+//  ForEveryoneAPI
+//
+//  Created by 송재훈 on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ForEveryoneAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
