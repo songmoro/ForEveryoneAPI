405b15e71abb36521ff095abbd8afe5a8e237f60
songmoro <wlqgkr@naver.com>
2023-11-09 21:40:16 +0900

    feat: 유저 목록 뷰 검색 추가

--- ForEveryoneAPI/UserListView.swift
+++ ForEveryoneAPI/UserListView.swift
@@ -8,43 +8,88 @@
 import SwiftUI
 
 struct UserListView: View {
+    @State var isSearchShow = false
+    
     var body: some View {
         VStack {
-            ForEach(0..<5) { _ in
-                NavigationLink {
-                    
-                } label: {
-                    Text("ID: Moro")
-                        .multilineTextAlignment(.leading)
-                }
-                .font(.title)
-                .padding()
-            }
+            UserList
             
             Spacer()
-            
-            HStack {
-                Button {
-                    
-                } label: {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.accentColor)
-                }
-                .disabled(true)
-                
-                Text("1 페이지")
-                
-                Button {
-                    
-                } label: {
-                    Image(systemName: "chevron.right")
-                        .foregroundColor(.accentColor)
-                }
-            }
-            .font(.title3)
-            .padding()
         }
         .navigationTitle("유저 목록")
+        .toolbar {
+            ToolbarItem {
+                SearchButton
+            }
+            
+            ToolbarItem(placement: .bottomBar) {
+                PageButton
+            }
+        }
+        .sheet(isPresented: $isSearchShow) {
+            SearchSheet
+        }
+    }
+    
+    var UserList: some View {
+        ForEach(0..<5) { _ in
+            NavigationLink {
+                
+            } label: {
+                Text("ID: Moro")
+                    .multilineTextAlignment(.leading)
+            }
+            .font(.title)
+            .padding()
+        }
+    }
+    
+    var SearchSheet: some View {
+        VStack {
+            HStack {
+                TextField(text: .constant("")) {
+                    Text("유저 명")
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("검색")
+                }
+            }
+        }
+        .padding()
+    }
+    
+    var SearchButton: some View {
+        Button {
+            isSearchShow = true
+        } label: {
+            Text("검색")
+        }
+    }
+    
+    var PageButton: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.accentColor)
+            }
+            .disabled(true)
+            
+            Text("1 페이지")
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.accentColor)
+            }
+        }
+        .font(.title3)
+        .padding()
     }
 }
 
