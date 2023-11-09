6d3c5671b9c7007108b4c449dedee77624658af7
songmoro <wlqgkr@naver.com>
2023-11-09 18:57:31 +0900

    feat: single user 추가

--- ForEveryoneAPI/Models.swift
+++ ForEveryoneAPI/Models.swift
@@ -7,3 +7,28 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var data: UserInfomation
+    var support: Support
+}
+
+struct UserInfomation: Codable {
+    var id: Int
+    var email: String
+    var firstName: String
+    var lastName: String
+    var avatar: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case email
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case avatar
+    }
+}
+
+struct Support: Codable {
+    var url: String
+    var text: String
+}
