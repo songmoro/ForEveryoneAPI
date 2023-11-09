ad8cccdb3f240c1e7c65d55d216221e9b3b7b25d
songmoro <wlqgkr@naver.com>
2023-11-09 20:26:35 +0900

    feat: api 열거형 추가

---
+++ ForEveryoneAPI/NetworkManger.swift
@@ -0,0 +1,97 @@
+//
+//  NetworkManger.swift
+//  ForEveryoneAPI
+//
+//  Created by 송재훈 on 11/9/23.
+//
+
+import SwiftUI
+import Moya
+
+enum API {
+    /// Get 유저 리스트
+    case GetUserList(page: Int? = nil, delay: Int? = nil)
+    /// Get 싱글 유저
+    case GetSingleUser(id: Int)
+    /// Post 유저
+    case CreateUser(name: String, job: String)
+    /// Patch 유저
+    case UpdateUser(id: Int, name: String, job: String)
+    /// Delete 유저
+    case DeleteUser(id: Int)
+    /// Post 가입
+    case Register(email: String, password: String? = nil)
+    /// Post 로그인
+    case Login(email: String, password: String? = nil)
+}
+
+extension API: TargetType {
+    var headers: [String : String]? {
+        switch self {
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+    
+    var baseURL: URL {
+        return URL(string: "https://reqres.in/api")!
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .GetUserList: return .get
+        case .GetSingleUser: return .get
+        case .CreateUser: return .post
+        case .UpdateUser: return .patch
+        case .DeleteUser: return .delete
+        case .Register: return .post
+        case .Login: return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .GetUserList: return "/users"
+        case .GetSingleUser(let id): return "/users/\(id)"
+        case .CreateUser: return "/users"
+        case .UpdateUser(let id, _, _): return "/users/\(id)"
+        case .DeleteUser(let id): return "/users/\(id)"
+        case .Register: return "/register"
+        case .Login: return "/login"
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .GetUserList(let page, let delay):
+            if let delay = delay {
+                return .requestParameters(parameters: ["delay": delay], encoding: URLEncoding.queryString)
+            }
+            else {
+                return .requestParameters(parameters: ["page": page!], encoding: URLEncoding.queryString)
+            }
+        case .GetSingleUser:
+            return .requestPlain
+        case .CreateUser(let name, let job):
+            return .requestJSONEncodable(["name": name, "job": job])
+        case .UpdateUser(_ , let name, let job):
+            return .requestJSONEncodable(["name": name, "job": job])
+        case .DeleteUser:
+            return .requestPlain
+        case .Register(let email, let password):
+            if let password = password {
+                return .requestJSONEncodable(["email": email, "password": password])
+            }
+            else {
+                return .requestJSONEncodable(email)
+            }
+        case .Login(let email, let password):
+            if let password = password {
+                return .requestJSONEncodable(["email": email, "password": password])
+            }
+            else {
+                return .requestJSONEncodable(["email": email])
+            }
+        }
+    }
+}
