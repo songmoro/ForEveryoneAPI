e7704c9ec06e1f75796e72c6f5098f975cbc3cb3
songmoro <wlqgkr@naver.com>
2023-11-10 23:38:53 +0900

    feat: 로그인 기능 추가

--- ForEveryoneAPI/MainView.swift
+++ ForEveryoneAPI/MainView.swift
@@ -9,22 +9,45 @@ import SwiftUI
 import Moya
 
 struct MainView: View {
+//    "email": "eve.holt@reqres.in"
+//    "password": "cityslicka"
+    @State var email = ""
+    @State var password = ""
+    @State var isCertified = false
+    @State var isAlertShow = false
+    @State var alertMessage = ""
+    
+    func login() {
+        NetworkManger.request(for: .Login(email: email, password: password), type: ResponseAccount.self) {
+            $0.map {
+                if $0.token != nil {
+                    isCertified = true
+                }
+                else {
+                    alertMessage = $0.error!
+                    isAlertShow = true
+                }
+            }
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             VStack {
                 InputField
                 AuthButton
             }
+            .alert(alertMessage, isPresented: $isAlertShow) { }
         }
     }
     
     var InputField: some View {
         Group {
-            TextField(text: .constant("")) {
+            TextField(text: $email) {
                 Text("아이디를 입력해주세요.")
             }
             
-            TextField(text: .constant("")) {
+            TextField(text: $password) {
                 Text("비밀번호를 입력해주세요.")
             }
         }
@@ -34,10 +57,13 @@ struct MainView: View {
     
     var AuthButton: some View {
         Group {
-            NavigationLink {
-                UserListView()
+            Button {
+                login()
             } label: {
                 Text("로그인")
+            }
+            .navigationDestination(isPresented: $isCertified) {
+                UserListView()
             }
             
             NavigationLink {
