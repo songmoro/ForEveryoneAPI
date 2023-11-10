fe55f25ec2b78d13633d29bc7ca6daaed583db02
songmoro <wlqgkr@naver.com>
2023-11-10 23:29:44 +0900

    feat: 상세 유저 뷰 추가

--- ForEveryoneAPI/DetailedUserView.swift
+++ ForEveryoneAPI/DetailedUserView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct DetailedUserView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            user
+        }
+        .toolbar {
+            ToolbarItem {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "ellipsis.circle")
+                }
+            }
+        }
     }
+    
+    var user: some View {
+        VStack {
+            AsyncImage(url: URL(string: "")) {
+                if let image = $0.image {
+                    image
+                }
+                else {
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+            }
+            .padding()
+            
+            Text("Email: ")
+            Text("name: ")
+        }
+    }
+    
+    //    var ContextButton: some View {
 }
 
 #Preview {
